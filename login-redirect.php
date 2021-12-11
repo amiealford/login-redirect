10b693ab1ce2b20a296dcd0d33f07e1eac95b611
Amie Alford <amiealford@gmail.com>
2021-12-11 14:45:27 -0500

    chore: add login-redirect.php file

---
+++ login-redirect.php
@@ -0,0 +1,21 @@
+<?php
+ 
+/*
+ 
+Plugin Name: Login Redirect
+ 
+Plugin URI: https://github.com/amiealford/test_theme_1
+ 
+Description: Plugin to accompany tutsplus guide to creating plugins, registers a post type.
+ 
+Version: 1.0
+ 
+Author: Amie Alford
+ 
+Author URI: https://github.com/amiealford/
+ 
+License: GPLv2 or later
+ 
+Text Domain: amiealford
+ 
+*/
